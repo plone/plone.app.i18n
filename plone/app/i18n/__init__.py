1fc39511e53aa949d1dd80dcaf438bf65c3740be
Hanno Schlichting <hanno@hannosch.eu>
2007-06-17 20:00:16 +0000

    Removed some left over tool init stuff. The local utilities are not used as tools anymore.
    svn path=/plone.app.i18n/trunk/; revision=15385

--- plone/app/i18n/__init__.py
+++ plone/app/i18n/__init__.py
@@ -1,15 +1,1 @@
-from Products.CMFCore.utils import ToolInit
-
-from plone.app.i18n.locales.countries import Countries
-from plone.app.i18n.locales.languages import ContentLanguages
-from plone.app.i18n.locales.languages import MetadataLanguages
-
-tools = (Countries, ContentLanguages, MetadataLanguages)
-
-def initialize(context):
-    """Intializer called when used as a Zope 2 product."""
-
-    ToolInit('plone.app.i18n',
-             tools=tools,
-             icon='tool.gif',
-    ).initialize(context)
+#
