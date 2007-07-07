e6fc4e9f53f72f479d6e206b3ac23502c27b4505
Daniel Nouri <daniel.nouri@gmail.com>
2007-07-07 20:11:20 +0000

    Half-baked stuff for Malthe to pick up (pong)
    svn path=/plone.app.i18n/branches/barcelona-sprint/; revision=15790

--- plone/app/i18n/messages.py
+++ plone/app/i18n/messages.py
@@ -1,12 +1,13 @@
-from zope.component import getUtility
+from zope import component
+from zope import interface
 from zope.i18n.interfaces import ITranslationDomain
 from zope.app.i18n.translationdomain import TranslationDomain
 from zope.app.i18n.interfaces import ILocalTranslationDomain
-
+from zope.i18n.testmessagecatalog import TestMessageCatalog
 from zope.component import getSiteManager
 
 def customize_translation(msgid, msg, domain, language):
-    translationdomain = getUtility(ITranslationDomain, name=domain)
+    translationdomain = component.getUtility(ITranslationDomain, name=domain)
 
     # if this domain is not persistent, we're going to register a new
     # local translation domain
@@ -18,3 +19,55 @@ def customize_translation(msgid, msg, domain, language):
 
     # lookup message catalog for this language
     translationdomain.addMessage(msgid, msg, language)
+
+def _match(msg, query):
+    return query in msg
+
+def query_message(query, language):
+    result = []
+    for name, util in component.getUtilitiesFor(ITranslationDomain):
+        for message in IListMessages(util).filter(language):
+            if _match(message['msgstr'], query):
+                result.append(message)
+    return result
+
+
+class GlobalTranslationDomainMessagesLister(object):
+    interface.implements(IListMessages)
+    component.adapts(ITranslationDomain)
+    
+    def __init__(self, td):
+        self.td = td
+
+    def __iter__(self):
+        for catalog in self.td._data.values():
+            mod_time = 0
+            if isinstance(catalog, TestMessageCatalog):
+                continue
+            for msgid, msgstr in catalog._catalog._catalog.items():
+                if msgid == '':
+                    continue
+                yield dict(msgid=msgid,
+                           msgstr=msgstr,
+                           domain=self.td.domain,
+                           language=catalog.language,
+                           mod_time=mod_time)
+
+    def filter(self, language):
+        return (message for message in self
+                if message['language'] == language)
+    
+
+class LocalTranslationDomainMessagesLister(object):
+    interface.implements(IListMessages)
+    component.adapts(ILocalTranslationDomain)
+
+    def __init__(self, td):
+        self.td = td
+
+    def __iter__(self):
+        return (message for message in self.td.getMessages())
+
+    def filter(self, language):
+        return (message for message in self
+                if message['language'] == language)
