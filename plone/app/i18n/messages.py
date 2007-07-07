386735ac7a9a384e65b5101b9b7937f1d9ceba68
Malthe Borch <mborch@gmail.com>
2007-07-07 17:57:41 +0000

    Added method to add custom translation message.
    svn path=/plone.app.i18n/branches/barcelona-sprint/; revision=15781

---
+++ plone/app/i18n/messages.py
@@ -0,0 +1,20 @@
+from zope.component import getUtility
+from zope.i18n.interfaces import ITranslationDomain
+from zope.app.i18n.translationdomain import TranslationDomain
+from zope.app.i18n.interfaces import ILocalTranslationDomain
+
+from zope.component import getSiteManager
+
+def customize_translation(msgid, msg, domain, language):
+    translationdomain = getUtility(ITranslationDomain, name=domain)
+
+    # if this domain is not persistent, we're going to register a new
+    # local translation domain
+    if not ILocalTranslationDomain.providedBy(translationdomain):
+        translationdomain = TranslationDomain()
+        translationdomain.domain = domain
+        sm = getSiteManager()
+        sm.registerUtility(translationdomain, ILocalTranslationDomain)
+
+    # lookup message catalog for this language
+    translationdomain.addMessage(msgid, msg, language)
