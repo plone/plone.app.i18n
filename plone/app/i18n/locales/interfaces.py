3cc7297d612bc082faf99262781d07a4fa622d2d
Ramon Navarro Bosch <ramon.nb@gmail.com>
2013-01-25 12:28:34 +0100

    Interfaces forgotten

--- plone/app/i18n/locales/interfaces.py
+++ plone/app/i18n/locales/interfaces.py
@@ -3,6 +3,8 @@ from plone.i18n.locales.interfaces import IContentLanguageAvailability
 from plone.i18n.locales.interfaces import IMetadataLanguageAvailability
 from plone.i18n.locales.interfaces import IModifiableCountryAvailability
 from plone.i18n.locales.interfaces import IModifiableLanguageAvailability
+from zope.interface import Interface
+
 
 class ICountries(ICountryAvailability, IModifiableCountryAvailability):
     """A modifiable list of countries."""
@@ -14,3 +16,12 @@ class IContentLanguages(IContentLanguageAvailability,
 class IMetadataLanguages(IMetadataLanguageAvailability,
                          IModifiableLanguageAvailability):
     """A modifiable list of available metadata languages."""
+
+class ISelectorAdapter(Interface):
+    """A marker interface for the selector viewlet"""
+
+    def languages(self):
+        """ Getting the list of languages available """
+
+    def urlForLanguage(self, code):
+        """ Getting the url for a language code """
