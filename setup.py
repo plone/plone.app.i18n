f14605f828d27dc938df2eb4e56fffc71312ceaf
Timo Stollenwerk <tisto@plone.org>
2014-06-28 09:56:13 +0200

    Declare PloneTestCase dependency.

--- setup.py
+++ setup.py
@@ -14,17 +14,22 @@ setup(name='plone.app.i18n',
           "License :: OSI Approved :: GNU General Public License (GPL)",
           "Operating System :: OS Independent",
           "Programming Language :: Python",
-        ],
+      ],
       keywords='plone i18n',
       author='Plone Foundation',
       author_email='plone-developers@lists.sourceforge.net',
       url='http://pypi.python.org/pypi/plone.app.i18n',
       license='GPL version 2',
       packages=find_packages(exclude=['ez_setup']),
-      namespace_packages = ['plone', 'plone.app'],
+      namespace_packages=['plone', 'plone.app'],
       include_package_data=True,
       zip_safe=False,
       install_requires=[
-        'setuptools',
+          'setuptools',
       ],
+      extras_require={
+          'test': [
+              'Products.PloneTestCase',
+          ]
+      },
       )
