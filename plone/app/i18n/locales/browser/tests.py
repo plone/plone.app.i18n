bf84944a0d368946f73faf0c5170f55826c8f99e
Philip Bauer <bauer@starzel.de>
2018-06-15 20:29:37 +0200

    fix tests for py3

--- plone/app/i18n/locales/browser/tests.py
+++ plone/app/i18n/locales/browser/tests.py
@@ -1,15 +1,26 @@
 # -*- coding: UTF-8 -*-
+from zope.component.testing import setUp, tearDown
 
+import doctest
+import re
+import six
 import unittest
 
-from zope.component.testing import setUp, tearDown
-import doctest
+
+class Py23DocChecker(doctest.OutputChecker):
+    def check_output(self, want, got, optionflags):
+        if six.PY2:
+            got = re.sub("u'(.*?)'", "'\\1'", got)
+        return doctest.OutputChecker.check_output(self, want, got, optionflags)
 
 
 def test_suite():
     return unittest.TestSuite((
-        doctest.DocTestSuite('plone.app.i18n.locales.browser.selector',
-                     setUp=setUp(),
-                     tearDown=tearDown,
-                     optionflags=doctest.ELLIPSIS | doctest.NORMALIZE_WHITESPACE),
+        doctest.DocTestSuite(
+            'plone.app.i18n.locales.browser.selector',
+            setUp=setUp(),
+            tearDown=tearDown,
+            optionflags=doctest.ELLIPSIS | doctest.NORMALIZE_WHITESPACE,
+            checker=Py23DocChecker(),
+            )
         ))
